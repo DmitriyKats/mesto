dd1b55a5542cd63ec81dcd78a9d0f437a54dbbcf
DmitriyKats <DimitriKatsIgorevich@gmail.com>
2021-05-23 11:57:03 +0300

    Релизная версия 2.1

--- scripts/main.js
+++ scripts/main.js
@@ -4,12 +4,22 @@ let popupModal = document.querySelector('.popup');
 
 let formElement = document.querySelector('.popup__form-container');
 let nameInput = formElement.querySelector('.popup__input_edit_name');
-let jobInput = formElement.querySelector('.popup__input_edit_description');
+let jobInput = formElement.querySelector('.popup__input_edit_vocation');
+
+let nameTitle = document.querySelector('.profile__title');
+let jobTitle = document.querySelector('.profile__subtitle');
 
 
 
 function ToggleClass() {
-  popupModal.classList.toggle('popup_opened');
+  if (popupModal.classList.contains('popup_opened') !== true) {
+    popupModal.classList.add('popup_opened');
+    nameInput.value = nameTitle.textContent;
+    jobInput.value = jobTitle.textContent;
+    
+  } else {
+    popupModal.classList.remove('popup_opened')
+  }
 }
 
 editButton.addEventListener('click', ToggleClass);
@@ -17,10 +27,7 @@ closeButton.addEventListener('click', ToggleClass);
 
 function formSubmitHandler(evt) {
   
-  let nameTitle = document.querySelector('.profile__title');
-  let jobTitle = document.querySelector('.profile__subtitle');
-
-  evt.preventDefault();
+    evt.preventDefault();
   
   nameTitle.textContent = nameInput.value;
   jobTitle.textContent = jobInput.value;
